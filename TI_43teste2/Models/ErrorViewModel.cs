069dbc40004f26ad8810aecfda053f53f994eb24
vinicio.gs19@hotmail.com <vinicio.gs19@hotmail.com>
2024-08-01 15:00:24 -0300

    Primeira versao

---
+++ TI_43teste2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TI_43teste2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
