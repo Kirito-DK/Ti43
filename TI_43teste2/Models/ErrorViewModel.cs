b8b6f5b62df7a5134bca6ee4abf5d7dd40fac4a0
vinicio.gs19@hotmail.com <vinicio.gs19@hotmail.com>
2024-08-08 17:13:46 -0300

    Criando Cliente

--- TI_43teste2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TI_43teste2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
